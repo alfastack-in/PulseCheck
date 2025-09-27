6b52c3a7433abc39f8b90d7b3f42f217009c6ee2
Prashant Agrawal <31651947+aa-prashant@users.noreply.github.com>
2025-09-27 17:17:42 +0530

    Add Slack interaction handler and tests

--- pulsecheck/pulse_check/doctype/pulsecheck_settings/test_pulsecheck_settings.py
+++ pulsecheck/pulse_check/doctype/pulsecheck_settings/test_pulsecheck_settings.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025, Prashant Agrawal and Contributors
 # See license.txt
 
-# import frappe
+import pytest
+
+frappe = pytest.importorskip("frappe")
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestPulsecheckSettings(FrappeTestCase):
-pass
+    pass
