75fddc5f2c66149f2463728c7af8a3127ffa29c7
Prashant Agrawal <31651947+aa-prashant@users.noreply.github.com>
2025-09-27 16:58:38 +0530

    Add posting date and validation to Weekly Checkin

--- pulsecheck/pulse_check/doctype/weekly_checkin/weekly_checkin.py
+++ pulsecheck/pulse_check/doctype/weekly_checkin/weekly_checkin.py
@@ -1,9 +1,37 @@
-# Copyright (c) 2025, Prashant Agrawal and contributors
-# For license information, please see license.txt
+"""Server-side logic for the Weekly Checkin DocType."""
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class WeeklyCheckin(Document):
-	pass
+    """Weekly progress check-in document."""
+
+    def before_insert(self):
+        """Populate posting date when the document is first created."""
+        if not self.posting_date:
+            self.posting_date = frappe.utils.today()
+
+        self._validate_progress_range()
+
+    def validate(self):
+        """Run document level validations prior to saving."""
+        self._validate_progress_range()
+
+    def _validate_progress_range(self):
+        """Ensure progress related values stay within a 0-100 range."""
+        for fieldname in ("progress_reported", "progress"):
+            value = getattr(self, fieldname, None)
+            if value in (None, ""):
+                continue
+
+            try:
+                numeric_value = float(value)
+            except (TypeError, ValueError):
+                label = fieldname.replace("_", " ").title()
+                frappe.throw(_("{0} must be a number between 0 and 100.").format(label))
+
+            if not 0 <= numeric_value <= 100:
+                label = fieldname.replace("_", " ").title()
+                frappe.throw(_("{0} must be between 0 and 100.").format(label))
